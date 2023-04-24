8fbcbada8502ac1c3f08845e239726c46f2c8aca
AgusIglesias72 <enpalabrass@gmail.com>
2023-04-24 12:47:18 -0300

    add form

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     './src/components/**/*.{js,ts,jsx,tsx}',
     './src/app/**/*.{js,ts,jsx,tsx}',
     './node_modules/flowbite/**/*.js',
+    './node_modules/flowbite-react/**/*.js',
   ],
   darkMode: 'class',
   theme: {
