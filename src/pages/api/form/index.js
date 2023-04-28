741a9cbcd3c6091a05dcf942bcbb144c53111618
AgusIglesias72 <enpalabrass@gmail.com>
2023-04-28 13:00:14 -0300

    probando

--- src/pages/api/form/index.js
+++ src/pages/api/form/index.js
@@ -3,14 +3,18 @@ import { appendData } from '@/utils/Google'
 export default async function handler(req, res) {
   const { method } = req
 
-  console.log(req.body)
-
   if (method == 'POST') {
     const { name, email, subject, message } = req.body
 
     const values = [[name, email, subject, message, new Date().toISOString()]]
     const table_name = 'Hoja 1'
-    await appendData(table_name, values)
-    res.status(200).json({ message: 'ok' })
+
+    try {
+      await appendData(table_name, values)
+      res.status(200).json({ message: 'ok' })
+    } catch (error) {
+      console.log(error)
+      res.status(500).json({ message: 'Error' })
+    }
   }
 }
