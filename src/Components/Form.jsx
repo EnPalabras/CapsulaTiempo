3d799f08b44f4767cc0e2346dd6f93781a318a4b
AgusIglesias72 <enpalabrass@gmail.com>
2023-05-05 14:03:01 -0300

    add date input

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -60,6 +60,12 @@ const options = {
 }
 
 export default function Form() {
+  const [loading, setLoading] = useState(false)
+  const [success, setSuccess] = useState(false)
+  const [error, setError] = useState(
+    'Oops! Algo salió mal. Intentá de nuevo más tarde'
+  )
+
   const [form, setForm] = useState({
     name: '',
     email: '',
@@ -73,23 +79,42 @@ export default function Form() {
 
   const handleSubmit = async (e) => {
     e.preventDefault()
+    setLoading(true)
+    setError(false)
 
-    const uploadForm = await fetch(
-      'https://capsulabackend-production.up.railway.app/api/capsula',
-      {
-        method: 'POST',
-        body: JSON.stringify(form),
-        headers: {
-          'Content-Type': 'application/json',
-        },
+    if (!form.name || !form.email || !form.message || !form.date) {
+      setError('Completa todos los campos')
+      setLoading(false)
+      return
+    }
+
+    if (form.email.indexOf('@') === -1) {
+      setError('El email no es válido')
+      setLoading(false)
+      return
+    }
+
+    try {
+      const uploadForm = await fetch(
+        'https://capsulabackend-production.up.railway.app/api/capsula',
+        {
+          method: 'POST',
+          body: JSON.stringify(form),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      )
+
+      const response = await uploadForm.json()
+      if (uploadForm.status === 200) {
+        setSuccess(true)
+      } else {
+        setError('Oops! Algo salió mal. Intentá de nuevo más tarde')
       }
-    )
-
-    const response = await uploadForm.json()
-    if (uploadForm.status === 200) {
-      console.log('Success')
-    } else {
-      console.log('Error')
+      setLoading(false)
+    } catch (error) {
+      setLoading(false)
     }
   }
 
@@ -115,6 +140,7 @@ export default function Form() {
           Acá (o en cualquier lado) podría ir un texto que explique de qué se
           trata la acción o lo que sea
         </p>
+
         <form className="space-y-8">
           <div className="flex flex-row w-full gap-8">
             <div className="w-1/2">
@@ -168,24 +194,6 @@ export default function Form() {
               required
             />
           </div>
-          {/* <div>
-            <label
-              for="subject"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-            >
-              Asunto del Mail
-            </label>
-            <input
-              type="text"
-              id="subject"
-              name="subject"
-              value={form.subject}
-              onChange={onInputChange}
-              className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-              placeholder="Let us know how we can help you"
-              required
-            />
-          </div> */}
           <div className="sm:col-span-2">
             <label
               for="message"
@@ -203,13 +211,51 @@ export default function Form() {
               placeholder="In a year"
             ></textarea>
           </div>
-          <button
-            type="submit"
-            onClick={handleSubmit}
-            className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-          >
-            Programar Mail
-          </button>
+          {error && (
+            <p className="text-red-500 text-sm font-semibold">{error}</p>
+          )}
+          {success ? (
+            <button
+              type="submit"
+              disabled
+              className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-green-600 sm:w-fit hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-primary-300 "
+            >
+              Mail Programado ✅
+            </button>
+          ) : loading ? (
+            <button
+              disabled
+              type="button"
+              class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 inline-flex items-center"
+            >
+              <svg
+                aria-hidden="true"
+                role="status"
+                class="inline w-4 h-4 mr-3 text-white animate-spin"
+                viewBox="0 0 100 101"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                  fill="#E5E7EB"
+                />
+                <path
+                  d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                  fill="currentColor"
+                />
+              </svg>
+              Loading...
+            </button>
+          ) : (
+            <button
+              type="submit"
+              onClick={handleSubmit}
+              className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 "
+            >
+              Programar Mail
+            </button>
+          )}
         </form>
       </div>
     </section>
